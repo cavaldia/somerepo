bea8736f340bd77dcf83cea3f6b31ad065c1721f
cavaldia <cavaldia@gmail.com>
2015-04-08 22:20:01 +0200

    Anything

---
+++ SomeTest/SomeTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeClass
+{
+    public class Time
+    {
+        int Year;
+        int Month;
+        int Date;
+        int Hour;
+        int Minute;
+        int Second;
+
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine("Metoda DisplayCurrentTime");
+        }
+    }
+
+    public class Tester
+    {
+        static void Main()
+        {
+            Time t = new Time();
+            t.DisplayCurrentTime();
+
+            Console.ReadKey();
+        }
+    }
+}
